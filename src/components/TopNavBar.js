ca2b75fea2b43d3da4ca4d1deb14f006a0b024ef
Shuyao222 <36521881+Shuyao222@users.noreply.github.com>
2019-11-11 20:47:54 -0500

    Add files via upload

---
+++ src/components/TopNavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../assets/images/nba-logoman-word-white.svg';
+
+export class TopNavBar extends React.Component {
+  render() {
+    return (
+      <header className="App-header">
+        <a  href='https://www.nba.com'>
+          <img src={logo} className="App-logo" alt="logo"/>
+        </a>
+      </header>
+    );
+  }
+}
